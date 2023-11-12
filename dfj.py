d6b3033e9d242c4e1f095589e7c24507cfb09672
lulagalvez <luisgalvezuk@gmail.com>
2023-11-12 05:53:35 -0300

    DFJ implemented

---
+++ dfj.py
@@ -0,0 +1,47 @@
+import pulp
+
+def dfj(c):
+    problem = pulp.LpProblem("ATSP", pulp.LpMinimize)
+
+    n = len(c)
+    V = range(n) # vertices
+    edges = [(i, j) for i in V for j in V if i != j] # aristas
+
+    x = pulp.LpVariable.dicts("x", edges, cat=pulp.LpBinary)
+
+    # Funcion objetivo
+    problem += pulp.lpSum(x[i, j] * c[i][j] for i in V for j in V if i != j)
+
+    # Restricciones
+    for i in V:
+        problem += pulp.lpSum(x[(i, j)] for j in V if i != j) == 1
+    for j in V:
+        problem += pulp.lpSum(x[(i, j)] for i in V if i != j) == 1
+
+    # Eliminando subtours
+    for s in range(2, n):
+        for i in range(n - s + 1):
+            subset = V[i:i + s]
+            problem += pulp.lpSum(x[i, j] for i in subset for j in subset if i != j) <= len(subset) - 1
+
+    problem.solve()
+
+    print("Millas nauticas que Oscarius Lulang debe recorrer =", pulp.value(problem.objective))
+    if pulp.LpStatus[problem.status] == 'Optimal':
+        current_port = 0 
+        visited_ports = set()
+        while True:
+            visited_ports.add(current_port)
+            next_port = None
+            for (i, j) in edges:
+                if x[(i, j)].varValue == 1 and i == current_port and j not in visited_ports:
+                    next_port = j
+                    break
+            if next_port is None:
+                break
+            print(f"De puerto {current_port} a puerto {next_port}")
+            current_port = next_port
+    else:
+        print("No es optimo")
+    
+
---
+++ main.py
@@ -0,0 +1,72 @@
+from dfj import dfj
+import os
+
+def list_instances():
+    instance_files = [f for f in os.listdir("instances/") if os.path.isfile(os.path.join("instances/", f))]
+    print("Instancias disponibles:")
+    for i, file_name in enumerate(instance_files, start=1):
+        print(f"{i}. {file_name}")
+    return instance_files
+
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        lines = file.readlines()
+
+    matrix_started = False
+    matrix = []
+
+    for line in lines:
+        if line.startswith("EDGE_WEIGHT_SECTION"):
+            matrix_started = True
+            continue
+        if matrix_started and line.strip() == "EOF":
+            break
+        if matrix_started:
+            row = [int(num) for num in line.split()]
+            matrix.append(row)
+
+    return matrix
+
+def main():
+    print("Elegir algoritmo:")
+    print("1. DFJ (Dantzig-Fulkerson-Johnson)")
+    print("2. GG (Gilmore-Gomory)")
+    print("3. MTZ (Miller-Tucker-Zemlin)")
+
+    algorithm_choice = input("Elegir el numero de algoritmo a ejecutar: ")
+
+    if algorithm_choice == "1":
+        algorithm_name = "DFJ"
+        solve_atsp = dfj
+    elif algorithm_choice == "2":
+        algorithm_name = "GG"
+        solve_atsp = gg
+    elif algorithm_choice == "3":
+        algorithm_name = "MTZ"
+        solve_atsp = mtz
+    else:
+        print("Error.")
+        return
+
+    print(f"Seleccionado algoritmo de {algorithm_name}.")
+
+    instance_files = list_instances()
+    if not instance_files:
+        print("No se encontraron instancias.")
+        return
+
+    file_choice = input("Ingresar numero de instancia: ")
+    try:
+        file_index = int(file_choice) - 1
+        selected_file = instance_files[file_index]
+    except (ValueError, IndexError):
+        print("Numero invalido.")
+        return
+
+    file_path = os.path.join("instances", selected_file)
+
+    distance_matrix = read_file(file_path)
+    solution = solve_atsp(distance_matrix)
+
+if __name__ == "__main__":
+    main()
